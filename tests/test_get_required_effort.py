1a555a543ec9310ca38ce1ac3e452a6dd9c3f26f
Nepo <braulio.rojas@islas.org.mx>
2020-07-10 00:04:36 -0700

    Agrega un ejemplo para cazar mutantes

--- tests/test_get_required_effort.py
+++ tests/test_get_required_effort.py
@@ -5,8 +5,11 @@ from eradication_success_assessment.get_required_effort import make_fit
 input_test: str = "tests/data/camaras_trampa_erradicacion_rata_natividad.csv"
 data: pd.DataFrame = pd.read_csv(input_test)
 capture_date = pd.to_datetime("2019-11-09")
+d: dict = {'Fecha': ["2019-11-09", "2019-11-08", "2019-11-10", "2019-11-11"], 'Cantidad_de_trampas_activas': [1, 2, 3, 4], 'Cantidad_de_avistamientos': [1, 0, 0, 0]}
+dates: pd.DataFrame = pd.DataFrame(data=d)
+output_tests = {'effort_without_sighted': 3, 'required_effort': 3, 'significance_level': 0.050000000000000044,'success_probability': 0.99}
 
 def test_make_fit():
-    make_fit(data, capture_date)
-    pass
+    output: dict = make_fit(dates, capture_date, True)
+    assert output==output_tests
 
