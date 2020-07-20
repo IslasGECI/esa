865c80921f3b6f5accba679b6a4a9aa222e0750c
Nepo <braulio.rojas@islas.org.mx>
2020-07-20 15:56:51 -0700

    Da formato con black

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,6 @@
 import subprocess
 
+
 def test_app_traps_camera__get_required_effort():
     bash_command = (
         f"python eradication_success_assessment/get_required_effort.py get-required-effort"
@@ -8,17 +9,12 @@ def test_app_traps_camera__get_required_effort():
 
 
 def test_app_traps__write_methodology():
-    bash_command = (
-        f"python eradication_success_assessment/get_required_effort.py write-methodology"
-    )
+    bash_command = f"python eradication_success_assessment/get_required_effort.py write-methodology"
     subprocess.check_call(bash_command, shell=True)
 
 
 def test_app_traps__version():
-    bash_command = (
-        f"python eradication_success_assessment/get_required_effort.py version"
-    )
+    bash_command = f"python eradication_success_assessment/get_required_effort.py version"
     subprocess.check_call(bash_command, shell=True)
     output = subprocess.getoutput(bash_command)
-    assert output == 'v0.1.0'
-
+    assert output == "v0.1.0"
