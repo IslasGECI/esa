18d9f02e59e27710cc46470fc69083f7d512f8e7
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2020-08-07 09:20:51 -0700

    Obten número de versíon con `pkg_resources` a partir de `setup.py`

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,5 @@
 import subprocess
+import pkg_resources
 
 
 def test_app_traps_camera__get_required_effort():
@@ -12,7 +13,8 @@ def test_app_traps__write_methodology():
 
 
 def test_app_traps__version():
+    expected_version = pkg_resources.require("eradication_success_assessment")[0].version
     bash_command = f"python eradication_success_assessment/get_required_effort.py version"
     subprocess.check_call(bash_command, shell=True)
-    output = subprocess.getoutput(bash_command)
-    assert output == "0.1.0"
+    obtained_version = subprocess.getoutput(bash_command)
+    assert obtained_version == expected_version
