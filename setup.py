f20f9f1c58316b4f1ddf7ade99edd40e432b3f96
Xavier Paredes <xavier.paredes@islas.org.mx>
2021-04-05 18:52:12 +0000

    🎨 verifica formato con black

--- setup.py
+++ setup.py
@@ -2,14 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="eradication_success_assessment",
-    version='0.1.0',
+    version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-    ],
-    entry_points={ 
-		"console_scripts": [
-        "traps_camera=eradication_success_assessment.get_required_effort:app"
-		]
-	}
+    install_requires=[],
+    entry_points={
+        "console_scripts": ["traps_camera=eradication_success_assessment.get_required_effort:app"]
+    },
 )
