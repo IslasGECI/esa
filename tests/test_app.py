d0e8b3fab6e418606ba45d1a44091f23a79ad875
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-12-01 16:08:17 -0800

    ✅⚡ Use typer test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,12 @@
 import subprocess
 import pkg_resources
 
+from typer.testing import CliRunner
+
+from eradication_success_assessment.get_required_effort import app
+
+runner = CliRunner()
+
 
 def test_app_traps_camera__get_required_effort():
     bash_command = "python eradication_success_assessment/get_required_effort.py get-required-effort --n-bootstrapping 30"
@@ -14,7 +20,5 @@ def test_app_traps__write_methodology():
 
 def test_app_traps__version():
     expected_version = pkg_resources.require("eradication_success_assessment")[0].version
-    bash_command = "python eradication_success_assessment/get_required_effort.py version"
-    subprocess.check_call(bash_command, shell=True)
-    obtained_version = subprocess.getoutput(bash_command)
-    assert obtained_version == expected_version
+    result = runner.invoke(app, ["version"])
+    assert expected_version in result.stdout
