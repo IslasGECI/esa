cd5cbc86f81b24283bf749552118ca75b688501e
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-12-02 16:13:48 -0800

    🏹👾✅ Change to the old version to hunt some mutants

--- tests/test_app.py
+++ tests/test_app.py
@@ -20,5 +20,6 @@ def test_app_traps__write_methodology(command):
 
 def test_app_traps__version():
     expected_version = pkg_resources.require("eradication_success_assessment")[0].version
-    result = runner.invoke(app, ["version"])
-    assert expected_version in result.stdout
+    bash_command = "python eradication_success_assessment/get_required_effort.py version"
+    obtained_version = subprocess.getoutput(bash_command)
+    assert obtained_version == expected_version
