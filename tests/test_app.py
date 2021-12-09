c15842987ff949cd2699ea244117d1b98a914c99
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-12-09 08:48:11 -0800

    ⚗️✅ Divide typer test in other file

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,5 @@
 import subprocess
 import pkg_resources
-import pytest
-
-from typer.testing import CliRunner
-
-from eradication_success_assessment.get_required_effort import app
 
 
 def test_app_traps__version():
@@ -13,15 +8,3 @@ def test_app_traps__version():
     subprocess.check_call(bash_command, shell=True)
     obtained_version = subprocess.getoutput(bash_command)
     assert obtained_version == expected_version
-
-
-runner = CliRunner()
-
-
-COMMANDS = ["write-methodology", "plot-histogram-effort", "version", "get-required-effort"]
-
-
-@pytest.mark.parametrize("command", COMMANDS)
-def test_app_traps__write_methodology(command):
-    result = runner.invoke(app, [command])
-    assert result.exit_code == 0
---
+++ tests/test_app_typer.py
@@ -0,0 +1,15 @@
+import pytest
+from typer.testing import CliRunner
+from eradication_success_assessment.get_required_effort import app
+
+
+runner = CliRunner()
+
+
+COMMANDS = ["write-methodology", "plot-histogram-effort", "version", "get-required-effort"]
+
+
+@pytest.mark.parametrize("command", COMMANDS)
+def test_app_traps__write_methodology(command):
+    result = runner.invoke(app, [command])
+    assert result.exit_code == 0
