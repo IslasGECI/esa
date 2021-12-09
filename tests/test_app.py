e53530598ee3762fe80dd5e979ef26a72ca5531f
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-12-09 15:31:37 -0800

    ✅⚗️ Use the cli

--- tests/test_app.py
+++ tests/test_app.py
@@ -14,7 +14,7 @@ def test_app_traps__write_methodology():
 
 def test_app_traps__version():
     expected_version = pkg_resources.require("eradication_success_assessment")[0].version
-    bash_command = "python eradication_success_assessment/get_required_effort.py version"
+    bash_command = "traps_camera version"
     subprocess.check_call(bash_command, shell=True)
     obtained_version = subprocess.getoutput(bash_command)
     assert obtained_version == expected_version
