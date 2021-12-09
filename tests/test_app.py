8dfad310dc9ea93bd0166a521e25c8e2758f85a7
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-12-09 09:19:36 -0800

    ✅⚗️ Return the old version

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,6 +2,16 @@ import subprocess
 import pkg_resources
 
 
+def test_app_traps_camera__get_required_effort():
+    bash_command = "python eradication_success_assessment/get_required_effort.py get-required-effort --n-bootstrapping 30"
+    subprocess.check_call(bash_command, shell=True)
+
+
+def test_app_traps__write_methodology():
+    bash_command = "python eradication_success_assessment/get_required_effort.py write-methodology"
+    subprocess.check_call(bash_command, shell=True)
+
+
 def test_app_traps__version():
     expected_version = pkg_resources.require("eradication_success_assessment")[0].version
     bash_command = "python eradication_success_assessment/get_required_effort.py version"
