5a7117ebbed3ccefb7f9d21fc19bf21b3b580d59
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-12-09 08:07:32 -0800

    ⚗️ Change order in the tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,6 +6,15 @@ from typer.testing import CliRunner
 
 from eradication_success_assessment.get_required_effort import app
 
+
+def test_app_traps__version():
+    expected_version = pkg_resources.require("eradication_success_assessment")[0].version
+    bash_command = "python eradication_success_assessment/get_required_effort.py version"
+    subprocess.check_call(bash_command, shell=True)
+    obtained_version = subprocess.getoutput(bash_command)
+    assert obtained_version == expected_version
+
+
 runner = CliRunner()
 
 
@@ -16,10 +25,3 @@ COMMANDS = ["write-methodology", "plot-histogram-effort", "version", "get-requir
 def test_app_traps__write_methodology(command):
     result = runner.invoke(app, [command])
     assert result.exit_code == 0
-
-
-def test_app_traps__version():
-    expected_version = pkg_resources.require("eradication_success_assessment")[0].version
-    bash_command = "python eradication_success_assessment/get_required_effort.py version"
-    obtained_version = subprocess.getoutput(bash_command)
-    assert obtained_version == expected_version
